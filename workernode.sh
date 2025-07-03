9362554e9a734c9399acb06d525a29b87d44c660
KOUSTUBH JUVEKAR <koustubhjuvekarofficial@gmail.com>
2025-07-03 21:03:12 +0530

    Create workernode.sh

---
+++ workernode.sh
@@ -0,0 +1,20 @@
+sudo su
+sudo swapoff -a
+sleep 5
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sleep 10
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sleep 5
+sudo apt-get update
+sleep 5
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
